ea2271f287940f621dd089093ba0d9aa937f1c80
stitipragyan barik <stitipragyanbarik@email.com>
2026-01-10 01:09:04 +0530

    error solve for rolecheck

--- src/routes/notice.routes.js
+++ src/routes/notice.routes.js
@@ -5,8 +5,8 @@ import {
   updateNotice,
   deleteNotice,
 } from "../controllers/notice.controller.js";
-import auth from "../middlewares/auth.js"; // existing authentication middleware
-import roleCheck from "../middlewares/role.js"; // restrict access to admin/coadmin
+import {auth} from "../middlewares/auth.js"; // existing authentication middleware
+import {permit} from "../middlewares/role.js"; // restrict access to admin/coadmin
 
 const router = express.Router();
 
@@ -14,12 +14,12 @@ const router = express.Router();
 router.get("/", auth, getAllNotices);
 
 //  Create a new notice (admin/coadmin only)
-router.post("/", auth, roleCheck, createNotice);
+router.post("/", auth, permit, createNotice);
 
 // Update a notice (admin/coadmin only)
-router.put("/:id", auth, roleCheck, updateNotice);
+router.put("/:id", auth, permit, updateNotice);
 
 // Delete a notice (admin/coadmin only)
-router.delete("/:id", auth, roleCheck, deleteNotice);
+router.delete("/:id", auth, permit, deleteNotice);
 
 export default router;
